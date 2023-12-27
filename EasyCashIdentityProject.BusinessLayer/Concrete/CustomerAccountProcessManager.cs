1a4ecea649d0592b2afe84d2c764273006351a70
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-12-27 22:15:43 +0300

    KullanıcıKayıt

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,45 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : IcustomerAccountProcessDal
+    {
+        private readonly IcustomerAccountProcessDal _customerAccountProcessDal;
+
+        public CustomerAccountProcessManager(IcustomerAccountProcessDal customerAccountProcessDal)
+        {
+            _customerAccountProcessDal = customerAccountProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.TDelete(t);
+        }
+
+        public CustomerAccountProcess TGetByID(int id)
+        {
+            return _customerAccountProcessDal.TGetByID(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+            return _customerAccountProcessDal.TGetList();
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+           _customerAccountProcessDal.TInsert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccountProcessDal.TUpdate(t);
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=DESKTOP-MJGGV3B;initial catalog=EasyCashDb;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountsProcess { get; set; }
+    }
+}
